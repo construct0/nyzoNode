3b61ed013e95646a8a0f1df892068b8710945fa0
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-17 00:28:36 +0100

    Create ICommand.cs

---
+++ Project/nyzoNode.Alignment.Core/Client/Commands/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyzoNode.Alignment.Core.Client.Commands; 
+internal interface ICommand {
+	string ShortCommand { get; }
+	string LongCommand { get; }
+	string Description { get; }
+	IList<string> ArgumentNames { get; }
+	IList<string> ArgumentIdentifiers { get; }
+	bool RequiresValidation { get; }
+	bool RequiresConfirmation { get; }
+	bool IsLongRunning { get; }
+	ValidationResult Validate(IList<string> argumentValues, CommandOutput commandOutput);
+	ExecutionResult Run(IList<string> argumentValues, CommandOutput commandOutput);
+}
