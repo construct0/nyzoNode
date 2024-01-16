ffca3421b417cc07fa97d0b4b731894b229657e9
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-16 20:21:59 +0100

    Scaffold empty class files

---
+++ Project/nyzoNode.Alignment.Core/Client/ClientTransactionUtil.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyzoNode.Alignment.Core.Client;
+
+internal class ClientTransactionUtil {
+}
---
+++ Project/nyzoNode.Alignment.Core/Client/Commands/Command.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyzoNode.Alignment.Core.Client.Commands; 
+internal class Command {
+}
