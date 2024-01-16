524646c2e03b831185d0bc39bf9a728bad2d9641
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-17 00:28:41 +0100

    Create DictionaryExtensions.cs

---
+++ Project/nyzoNode.Alignment.Core/DictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace nyzoNode.Alignment.Core;
+
+public static class DictionaryExtensions {
+	public static TValue? TryGetByteBufferValue<TKey, TValue>(this Dictionary<TKey, TValue> dict, ByteBuffer
+		 byteBuffer, TValue? defaultValue=default) where TKey : ByteBuffer {
+		foreach(var kvp in dict) {
+			if(kvp.Key.IsEqualToContentInByteBuffer(byteBuffer)) {
+				return kvp.Value;
+			}
+		}
+
+		return defaultValue;
+	}
+}
