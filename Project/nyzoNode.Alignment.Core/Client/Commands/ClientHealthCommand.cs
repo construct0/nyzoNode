a082200fc5f18df5a0d3f555f703aae25d20efc2
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-17 15:49:07 +0100

    Update ClientHealthCommand.cs

--- Project/nyzoNode.Alignment.Core/Client/Commands/ClientHealthCommand.cs
+++ Project/nyzoNode.Alignment.Core/Client/Commands/ClientHealthCommand.cs
@@ -1,10 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace nyzoNode.Alignment.Core.Client.Commands;
 
-namespace nyzoNode.Alignment.Core.Client.Commands;
+internal class ClientHealthCommand : ICommand {
+	public string ShortCommand => "HTH";
 
-internal class ClientHealthCommand {
+	public string LongCommand => "health";
+
+	public string Description => "display health metrics";
+
+	public IList<string> ArgumentNames { get; init; } = new List<string>();
+
+	public IList<string> ArgumentIdentifiers { get; init; } = new List<string>();
+
+	public bool RequiresValidation => false;
+
+	public bool RequiresConfirmation => false;
+
+	public bool IsLongRunning => false;
+
+	public ExecutionResult Run(IList<string> argumentValues, CommandOutput commandOutput) {
+		// Make the lists for the notices and errors & make the result command table
+		var notices = new List<string>();
+		var errors = new List<string>();
+		CommandTable commandTable = new(
+			new CommandTableHeader("amt of nodes in mesh", "numberNodesInMesh"),
+			new CommandTableHeader("amt of preferred nodes", "numberOfPreferredNodesInMesh"),
+			new CommandTableHeader("amt of non-preferred fetches", "numberOfNonPreferredFetches"),
+			new CommandTableHeader("amt of preferred fetches", "numberOfPreferredFetches"),
+			new CommandTableHeader("amt of successful block fetches", "numberOfSuccessfulBlockFetches"),
+			new CommandTableHeader("amt of consecutive successful block fetches", "numberOfConsecutiveSuccessfulBlockFetches"),
+			new CommandTableHeader("amt of unsuccessful block fetches", "numberOfUnsuccessfulBlockFetches"),
+			new CommandTableHeader("amt of consecutive unsuccessful block fetches", "numberOfConsecutiveUnsuccessfulBlockFetches"),
+			new CommandTableHeader("frozen edge verification age (ms)", "frozenEdgeVerificationAgeMilliseconds")
+		);
+
+		commandTable.setInvertedRowsAndColumns(true);
+
+		// Add results to command table
+		Block frozenEdge = BlockManager.FrozenEdge;
+		commandTable.AddRow(
+			ClientNodeManager.NumberOfNodesInMesh,
+			ClientNodeManager.NumberOfPreferredNodesInMesh,
+			ClientNodeManager.numberOfNonPreferredFetches,
+			ClientNodeManager.numberOfPreferredFetches,
+			ClientNodeManager.numberOfSuccessfulBlockFetches,
+			ClientNodeManager.numberOfConsecutiveSuccessfulBlockFetches,
+			ClientNodeManager.numberOfUnsuccessfulBlockFetches,
+			ClientNodeManager.numberOfConsecutiveUnsuccessfulBlockFetches,
+			frozenEdge is null 
+				? Double.NaN 
+				: (DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - frozenEdge.VerificationTimestamp)
+		);
+
+		// Frozen edge error
+		if(frozenEdge is null) {
+			errors.Add("Unable to get frozen edge");
+		}
+
+		return new SimpleExecutionResult(notices, errors, commandTable);
+	}
+
+	public ValidationResult Validate(IList<string> argumentValues, CommandOutput commandOutput) {
+		return null!;
+	}
 }
