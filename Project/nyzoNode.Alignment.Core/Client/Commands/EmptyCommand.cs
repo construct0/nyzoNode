b37f266d489912a9d9e0c12a9a7eac270d5e7daa
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-17 22:26:16 +0100

    Update EmptyCommand.cs

--- Project/nyzoNode.Alignment.Core/Client/Commands/EmptyCommand.cs
+++ Project/nyzoNode.Alignment.Core/Client/Commands/EmptyCommand.cs
@@ -6,5 +6,33 @@ using System.Threading.Tasks;
 
 namespace nyzoNode.Alignment.Core.Client.Commands;
 
-internal class EmptyCommand {
+internal class EmptyCommand : ICommand {
+	public string ShortCommand => "";
+
+	public string LongCommand => "empty";
+
+	public string Description => "placeholder command handler used when an empty command is provided | received";
+
+	public IList<string> ArgumentNames { get; init; } = new List<string>() {
+		""
+	};
+
+	public IList<string> ArgumentIdentifiers { get; init; } = new List<string>() {
+		""
+	};
+
+	public bool RequiresValidation => false;
+
+	public bool RequiresConfirmation => false;
+
+	public bool IsLongRunning => false;
+
+	public ExecutionResult Run(IList<string> argumentValues, CommandOutput commandOutput) {
+		CommandManager.PrintCommands(commandOutput));
+		return null!;
+	}
+
+	public ValidationResult Validate(IList<string> argumentValues, CommandOutput commandOutput) {
+		return null!;
+	}
 }
