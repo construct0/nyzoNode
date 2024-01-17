bd584c183416d18afb4257ca998e14cbf4ba0c5b
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2024-01-17 22:26:11 +0100

    Update ExitCommand.cs

--- Project/nyzoNode.Alignment.Core/Client/Commands/ExitCommand.cs
+++ Project/nyzoNode.Alignment.Core/Client/Commands/ExitCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using nyzoNode.Alignment.Core.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,39 @@ using System.Threading.Tasks;
 namespace nyzoNode.Alignment.Core.Client.Commands;
 
 internal class ExitCommand {
+	public string ShortCommand => "X";
+
+	public string LongCommand => "exit";
+
+	public string Description => "exit Nyzo client";
+
+	public IList<string> ArgumentNames { get; init; } = new List<string>();
+
+	public IList<string> ArgumentIdentifiers { get; init; } = new List<string>();
+
+	public bool RequiresValidation => false;
+
+	public bool RequiresConfirmation => false;
+
+	public bool IsLongRunning => false;
+
+	public ExecutionResult Run(IList<string> argumentValues, CommandOutput commandOutput) {
+		string message = "Thank you for using the Nyzo client!";
+		
+		ConsoleUtil.PrintTable(
+			new List<List<string>> {
+				new List<string> {
+					message
+				}
+			}
+		);
+
+		UpdateUtil.Terminate();
+
+		return null!;
+	}
+
+	public ValidationResult Validate(IList<string> argumentValues, CommandOutput commandOutput) {
+		return null!;
+	}
 }
